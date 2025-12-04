21c91aed31ee54f1e93ae2dc418b89cc009681e1
Arafat Mahmud <arafat746384@gmail.com>
2025-12-04 17:14:56 +0600

    user and password hide

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 
 // Middleware
@@ -16,7 +17,7 @@ app.get('/', (req, res) => {
 
 
 
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.3tpdxno.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3tpdxno.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
